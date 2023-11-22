6273eae25a15b2c414b9cc189c563386c7e77af0
Shanksus <devalent20@gmail.com>
2023-11-22 19:44:17 +0100

    Navegación hecha

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import StyledText from './StyledText.jsx'
 import Constants from 'expo-constants'
 import theme from '../theme.js'
+import { Link, useLocation } from 'react-router-native'
+import { TouchableWithoutFeedback } from 'react-native-web'
 
 const styles = StyleSheet.create({
     container: {
@@ -10,16 +12,47 @@ const styles = StyleSheet.create({
         paddingTop: Constants.statusBarHeight + 10,
         paddingBottom: 10,
         paddingLeft: 10,
+        flexDirection: 'row',
     },
     text: {
-        color: theme.appBar.primartyText
+        color: theme.appBar.textSecondary,
+        paddingHorizontal: 10
+    },
+    scroll: {
+        paddingBottom: 15,
+    },
+    active: {
+        color: theme.appBar.textPrimary,                
     }
 })
 
+const AppBarTab = ({ children, to }) => {
+    
+    const { pathname } = useLocation() // Obtain the current path
+
+    const active = pathname === to  // Determines the active tab
+
+    const textStyles = [
+        styles.text,
+        active && styles.active
+    ]
+
+    return (
+        <Link to={to} >
+            <StyledText fontWeight='bold' style={textStyles}>
+                {children}
+            </StyledText>
+        </Link>
+    )
+}
+
 const AppBar = () => {
     return (
-        <View style = {styles.container}>
-            <StyledText fontWeight='bold' style={styles.text}> Repositories</StyledText>
+        <View style={styles.container}>
+            <ScrollView horizontal style={styles.scroll}>
+                <AppBarTab active to='/'>Repositories</AppBarTab>
+                <AppBarTab to='/signin'>Sigin</AppBarTab>
+            </ScrollView>
         </View>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { View } from 'react-native'
-import Constants from 'expo-constants'
+import { View, Text } from 'react-native'
 import RepositoryList from './RepositoryList'
 import AppBar from './AppBar.jsx'
-
+import { Redirect, Route, Switch, Routes } from 'react-router-native'
 
 const Main = () => {
     return (
-        <View style={{flex: 1 }}>
-            <AppBar/>
-            <RepositoryList/>
+        <View style={{ flex: 1 }}>
+            <AppBar />
+            <Routes>
+                <Route path='/' element={<RepositoryList />} />
+                <Route path='/signin' element={<Text>Sign In</Text>} />
+            </Routes>
         </View>
     )
 }
